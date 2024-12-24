e816fde2591e1e98924a5372ff8fb277c9526259
MdAjams <mdajamansari099gmailgit config --global user.email mdajamansari099gmail>
2024-12-24 15:15:22 +0530

    Added functionality to find even and odd numbers

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 console.log("This is a simle project")
 
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+const oddNumbers = numbers.filter(num => num % 2 !== 0);
+
+console.log("Even Numbers:", evenNumbers);
+console.log("Odd Numbers:", oddNumbers);
